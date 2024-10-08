b989e9df2e1da96e4ecef8676bfe7bd85172071a
Sam Patterson <pattersonrsam@gmail.com>
2024-10-07 23:48:44 -0400

    remove name param

--- src/app/chat/[id]/[name]/page.tsx
+++ src/app/chat/[id]/[name]/page.tsx
@@ -1,9 +1,5 @@
 import ChatRoom from "@/components/ChatRoom";
 
-export default function ChatPage({
-  params,
-}: {
-  params: { id: string; name: string };
-}) {
-  return <ChatRoom id={params.id} name={params.name} />;
+export default function ChatPage({ params }: { params: { id: string } }) {
+  return <ChatRoom id={params.id} />;
 }
