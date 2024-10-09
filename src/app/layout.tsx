683d207f1ddb89e5e4f2be5afacbd9223bffe190
Sam Patterson <pattersonrsam@gmail.com>
2024-10-08 23:57:22 -0400

    SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,39 @@
-import { Metadata } from "next";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Voynich",
-  description: "Secure, encrypted chat application",
+  title: "Voynich - Secure Encrypted Ephemeral Messaging",
+  description:
+    "Voynich offers secure, encrypted, and ephemeral messaging. Create temporary chat rooms with end-to-end encryption for private conversations.",
+  keywords:
+    "Voynich, encrypted messaging, encrypted chat, ephemeral messaging, secure chat, private messaging",
+  openGraph: {
+    title: "Voynich - Secure Encrypted Ephemeral Messaging",
+    description:
+      "Create secure, encrypted, and temporary chat rooms with Voynich.",
+    url: "https://voynich.app",
+    siteName: "Voynich",
+    images: [
+      {
+        url: "https://voynich.app/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Voynich Encrypted Messaging",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Voynich - Secure Encrypted Ephemeral Messaging",
+    description:
+      "Create secure, encrypted, and temporary chat rooms with Voynich.",
+    images: ["https://voynich.app/twitter-image.jpg"],
+  },
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import Image from "next/image";
-import Landing from "../components/landing";
+import Landing from "@/components/landing";
+import Script from "next/script";
+
 export default function Home() {
   return (
-    <div>
+    <main>
       <Landing />
-    </div>
+      <Script id="structured-data" type="application/ld+json">
+        {`
+          {
+            "@context": "https://schema.org",
+            "@type": "SoftwareApplication",
+            "name": "Voynich",
+            "applicationCategory": "CommunicationApplication",
+            "operatingSystem": "Web",
+            "description": "Voynich is a secure, encrypted, and ephemeral messaging application for private conversations.",
+            "offers": {
+              "@type": "Offer",
+              "price": "0"
+            }
+          }
+        `}
+      </Script>
+    </main>
   );
 }
